dbd92acecbfb5bc6c56c30be889f72f7f5a6300e
Daniel Weiß <wanieldeiss@gmail.com>
2020-09-01 08:18:03 +0200

    first small refactoring

--- src/components/RandomWords.jsx
+++ src/components/RandomWords.jsx
@@ -1,3 +1,4 @@
+import { first } from 'lodash';
 // Node Packages
 import React, { Component } from 'react';
 
@@ -5,11 +6,9 @@ import React, { Component } from 'react';
 import Adjectives from '../data/adjectives';
 import Nouns from '../data/nouns';
 
-const RandomNumber = num => Math.floor((Math.random() * Math.floor(num)));
-const GetGenderSuffix = (gender, adjEnding) => {
-
+const randomNumber = (num) => Math.floor(Math.random() * Math.floor(num));
+const getGenderSuffix = (gender, adjEnding) => {
   const prefix = adjEnding == 'a' ? 'n' : '';
-
   switch (gender) {
     case 'mas':
       return `${prefix}er`;
@@ -20,26 +19,39 @@ const GetGenderSuffix = (gender, adjEnding) => {
   }
 };
 
+const getRandomNoun = () => {
+  const randomNounNumber = randomNumber(Nouns.length);
+  return Nouns[randomNounNumber];
+};
+
+const getRandomAdjective = () => {
+  const randomAdjectiveNumber = randomNumber(Adjectives.length);
+  return Adjectives[randomAdjectiveNumber];
+};
+
 const GetWords = () => {
-  const RAN_NOUN = RandomNumber(Nouns.length);
-  const RAN_ADJECTIVE = RandomNumber(Adjectives.length);
-  const RAN_ADJECTIVE_2 = RandomNumber(Adjectives.length);
+  const noun = getRandomNoun();
+  const firstRandomAdjective = getRandomAdjective();
+  const firstAdjectiveEnding = firstRandomAdjective.slice(-1);
+  const firstGenderSuffix = getGenderSuffix(noun.gender, firstAdjectiveEnding);
+  const firstAdjective = firstRandomAdjective + firstGenderSuffix;
+  const secondRandomAdjective = getRandomAdjective();
+  const secondAdjectiveEnding = secondRandomAdjective.slice(-1);
+  const secondGenderSuffix = getGenderSuffix(
+    noun.gender,
+    secondAdjectiveEnding
+  );
+  const secondAdjective = secondRandomAdjective + secondGenderSuffix;
 
-  const NOUN = Nouns[RAN_NOUN];
-
-  const RAN_ADJ_ORG = Adjectives[RAN_ADJECTIVE];
-  const ADJECTIVE = RAN_ADJ_ORG + GetGenderSuffix(NOUN.gender, RAN_ADJ_ORG.slice(-1));
-
-  const RAN_ADJ2_ORG = Adjectives[RAN_ADJECTIVE_2];
-  const ADJECTIVE_2 = RAN_ADJ2_ORG + GetGenderSuffix(NOUN.gender, RAN_ADJ2_ORG.slice(-1));
-
-  return `Du ${ADJECTIVE} ${NOUN.word}, ${ADJECTIVE_2}!`;
+  return `Du ${firstAdjective} ${noun.word}, ${secondAdjective}!`;
 };
 
 const RandomWords = () => {
   return (
     <div className="RandomWords">
-      <h1><GetWords /></h1>
+      <h1>
+        <GetWords />
+      </h1>
     </div>
   );
 };
