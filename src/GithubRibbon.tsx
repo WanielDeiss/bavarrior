2885fe918fe84d4863b0bb37ed9d1c01ab7d7a5d
Daniel Weiß <wanieldeiss@gmail.com>
2020-09-01 19:15:54 +0200

    ja, host recht... des heart si besser o

--- src/GithubRibbon.tsx
+++ src/GithubRibbon.tsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 const GithubRibbon = () => {
-  const ghUser = 'WanielDeiss';
-  const gbRepo = 'bavarrior';
+  const githubUser = 'WanielDeiss';
+  const githubRepo = 'bavarrior';
 
   return (
-    <a href={`https://github.com/${ghUser}/${gbRepo}`}>
+    <a href={`https://github.com/${githubUser}/${githubRepo}`}>
       <img
         style={{ position: 'absolute', top: 0, right: 0, border: 0 }}
         src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"
